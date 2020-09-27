28dcade404ef43d26d12298f6e2134bb24f62b88
Ernest Chang <iattempt.net@gmail.com>
2020-09-27 21:25:56 +0800

    test: add tests

--- cypress/integration/behavior/close.spec.js
+++ cypress/integration/behavior/close.spec.js
@@ -7,11 +7,27 @@ context('close', () => {
     cy.get('.vue-dropdown').should('not.exist')
   })
 
+  it('should close on blur', () => {
+    cy.visit('/cypress/fixtures/open-and-close/without-searchable-and-taggable.html')
+
+    cy.get('.vue-select').click()
+    cy.get('body').click()
+    cy.get('.vue-dropdown').should('not.exist')
+  })
+
   it('should close when using searchable prop', () => {
     cy.visit('/cypress/fixtures/open-and-close/with-searchable.html')
 
     cy.get('.vue-select').click()
     cy.get('.icon.arrow-downward').click()
+    cy.get('.vue-dropdown').should('not.exist')
+  })
+
+  it('should close when using searchable prop', () => {
+    cy.visit('/cypress/fixtures/open-and-close/with-searchable.html')
+
+    cy.get('.vue-select').click()
+    cy.get('body').click()
     cy.get('.vue-dropdown').should('not.exist')
   })
 
@@ -23,11 +39,27 @@ context('close', () => {
     cy.get('.vue-dropdown').should('not.exist')
   })
 
+  it('should close when using taggable prop', () => {
+    cy.visit('/cypress/fixtures/open-and-close/with-taggable.html')
+
+    cy.get('.vue-select').click()
+    cy.get('body').click()
+    cy.get('.vue-dropdown').should('not.exist')
+  })
+
   it('should close when using searchable and taggable prop', () => {
     cy.visit('/cypress/fixtures/open-and-close/with-searchable-and-taggable.html')
 
     cy.get('.vue-select').click()
     cy.get('.icon.arrow-downward').click()
+    cy.get('.vue-dropdown').should('not.exist')
+  })
+
+  it('should close when using searchable and taggable prop', () => {
+    cy.visit('/cypress/fixtures/open-and-close/with-searchable-and-taggable.html')
+
+    cy.get('.vue-select').click()
+    cy.get('body').click()
     cy.get('.vue-dropdown').should('not.exist')
   })
 })
