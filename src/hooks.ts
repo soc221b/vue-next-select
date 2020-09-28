b815ced0f9d0fc1f0278f4e96b350f344e5c987c
Ernest Chang <iattempt.net@gmail.com>
2020-09-28 19:42:47 +0800

    fix: it has not rendered

--- src/hooks.ts
+++ src/hooks.ts
@@ -4,8 +4,10 @@ import { Ref } from 'vue'
 export const useHeight = (element: Ref<Element>, watchSource: any) => {
   const height = ref('0')
   const calcHeaderHeight = () => {
-    if (!element.value) return
-    height.value = window.getComputedStyle(element.value).height
+    setTimeout(() => {
+      if (!element.value) return
+      height.value = window.getComputedStyle(element.value).height
+    })
   }
   watch(watchSource, calcHeaderHeight)
   onMounted(calcHeaderHeight)
