a39e4fbe282b95d884866e152b4033b3da31461f
Ernest Chang <iattempt.net@gmail.com>
2020-09-30 23:41:50 +0800

    test: add tests

--- cypress/integration/props/model.spec.js
+++ cypress/integration/props/model.spec.js
@@ -2,9 +2,83 @@
 
 context('model', () => {
   it('should update when model changed', () => {
-    cy.visit('/cypress/fixtures/model/change-model.html')
+    cy.visit('/cypress/fixtures/model/single/change-model.html')
     cy.get('.vue-select').click()
 
+    cy.get('.vue-dropdown-item.selected').should('have.length', 0)
+
+    cy.get('#previous-button').click()
+    cy.get('.vue-dropdown-item.selected').should('have.length', 1)
+    cy.get('.vue-dropdown-item').first().should('have.class', 'selected')
+
+    cy.get('#previous-button').click()
+    cy.get('.vue-dropdown-item.selected').should('have.length', 1)
+    cy.get('.vue-dropdown-item').first().next().should('have.class', 'selected')
+
+    cy.get('#previous-button').click()
+    cy.get('.vue-dropdown-item.selected').should('have.length', 1)
+    cy.get('.vue-dropdown-item').first().next().next().should('have.class', 'selected')
+
+    cy.get('#previous-button').click()
+    cy.get('.vue-dropdown-item.selected').should('have.length', 0)
+  })
+
+  it('should update when model changed (object)', () => {
+    cy.visit('/cypress/fixtures/model/single/change-model-with-object.html')
+    cy.get('.vue-select').click()
+
+    cy.get('.vue-dropdown-item.selected').should('have.length', 0)
+
+    cy.get('#previous-button').click()
+    cy.get('.vue-dropdown-item.selected').should('have.length', 1)
+    cy.get('.vue-dropdown-item').first().should('have.class', 'selected')
+
+    cy.get('#previous-button').click()
+    cy.get('.vue-dropdown-item.selected').should('have.length', 1)
+    cy.get('.vue-dropdown-item').first().next().should('have.class', 'selected')
+
+    cy.get('#previous-button').click()
+    cy.get('.vue-dropdown-item.selected').should('have.length', 1)
+    cy.get('.vue-dropdown-item').first().next().next().should('have.class', 'selected')
+
+    cy.get('#previous-button').click()
+    cy.get('.vue-dropdown-item.selected').should('have.length', 0)
+  })
+})
+
+context('model (multiple)', () => {
+  it('should update when model changed', () => {
+    cy.visit('/cypress/fixtures/model/multiple/change-model.html')
+    cy.get('.vue-select').click()
+
+    cy.get('.vue-dropdown-item.selected').should('have.length', 0)
+    cy.get('.vue-tag.selected').should('have.length', 0)
+
+    cy.get('#previous-button').click()
+    cy.get('.vue-dropdown-item.selected').should('have.length', 1)
+    cy.get('.vue-tag.selected').should('have.length', 1)
+
+    cy.get('#previous-button').click()
+    cy.get('.vue-dropdown-item.selected').should('have.length', 2)
+    cy.get('.vue-tag.selected').should('have.length', 2)
+
+    cy.get('#previous-button').click()
+    cy.get('.vue-dropdown-item.selected').should('have.length', 3)
+    cy.get('.vue-tag.selected').should('have.length', 3)
+
+    cy.get('#previous-button').click()
+    cy.get('.vue-dropdown-item.selected').should('have.length', 0)
+    cy.get('.vue-tag.selected').should('have.length', 0)
+  })
+
+  it('should update when model changed (object)', () => {
+    cy.visit('/cypress/fixtures/model/multiple/change-model-with-object.html')
+    cy.get('.vue-select').click()
+
+    cy.get('.vue-dropdown-item.selected').should('have.length', 0)
+    cy.get('.vue-tag.selected').should('have.length', 0)
+
+    cy.get('#previous-button').click()
     cy.get('.vue-dropdown-item.selected').should('have.length', 1)
     cy.get('.vue-tag.selected').should('have.length', 1)
 
