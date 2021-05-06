981c16175d6fb7e8ad85cae23d40c8f98523cda7
Ernest <iendeavor.github.io@gmail.com>
2021-05-06 08:50:54 +0800

    docs: update

--- examples/src/route.ts
+++ examples/src/route.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 export const routes = [
   { path: '/basic-usage', component: () => import('./views/BasicUsage.vue') },
   { path: '/creatable', component: () => import('./views/Creatable.vue') },
+  { path: '/select-all', component: () => import('./views/SelectAll.vue') },
   { path: '/limiting-selections', component: () => import('./views/LimitingSelections.vue') },
   { path: '/group', component: () => import('./views/Group.vue') },
   { path: '/remote-searching', component: () => import('./views/RemoteSearching.vue') },
