4696a1651f71bc3d489638a5073844ca4fef2d61
Ernest <iendeavor.github.io@gmail.com>
2021-05-08 14:45:31 +0800

    docs: update

--- examples/src/route.ts
+++ examples/src/route.ts
@@ -1,15 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-export const routes = [
-  { path: '/basic-usage', component: () => import('./views/BasicUsage.vue') },
-  { path: '/creatable', component: () => import('./views/Creatable.vue') },
-  { path: '/select-all', component: () => import('./views/SelectAll.vue') },
-  { path: '/limiting-selections', component: () => import('./views/LimitingSelections.vue') },
-  { path: '/group', component: () => import('./views/Group.vue') },
-  { path: '/remote-searching', component: () => import('./views/RemoteSearching.vue') },
-  { path: '/custom-dropdown', component: () => import('./views/CustomDropdown.vue') },
-  { path: '/custom-tag', component: () => import('./views/CustomTag.vue') },
+const viewNames = [
+  'Basic usage',
+  'Creatable',
+  'Select all',
+  'Vuex',
+  'Limiting selections',
+  'Group',
+  'Remote searching',
+  'Custom dropdown',
+  'Custom tag',
 ]
+
+export const routes = viewNames.map(viewName => ({
+  path: `/${viewName}`,
+  component: () =>
+    import(
+      `./views/${viewName
+        .split(' ')
+        .map(part => part.charAt(0).toUpperCase() + part.slice(1))
+        .join('')}.vue`
+    ),
+}))
 
 export default createRouter({
   history: createWebHistory('vue-next-select'),
