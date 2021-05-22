37635a3daecb6e15e502eb5f06ebb3659d4a07cf
Ernest <iendeavor.github.io@gmail.com>
2021-05-22 15:24:50 +0800

    docs: fix example

--- examples/src/views/RemoteSearching.vue
+++ examples/src/views/RemoteSearching.vue
@@ -1,10 +1,11 @@
 <template>
-  <pre>modelValue: {{ JSON.stringify(selectedUser) }}</pre>
+  <pre>modelValue: {{ JSON.stringify(modelValue) }}</pre>
 
   <vue-select
-    v-model="selectedUser"
-    :options="users"
+    v-model="modelValue"
+    :options="options"
     searchable
+    multiple
     clear-on-close
     :loading="loading"
     @search:input="handleInput"
@@ -15,34 +16,35 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 
+const userNames = ref([])
+fetch('https://randomuser.me/api/?seed=0&results=1000&inc=name').then(async result => {
+  const userResult = (((await result.json()) as any).results ?? []).map(
+    ({ name: { first, last } }) => first + ' ' + last,
+  )
+  userNames.value = [...new Set(userResult)]
+})
+
 export default defineComponent({
   setup() {
-    const selectedUser = ref(null)
-
-    const users = ref([])
+    const modelValue = ref([])
+    const options = ref([])
 
     const loading = ref(false)
-
     async function handleInput(inputEvent) {
-      if (inputEvent.target.value === '') return
+      if (inputEvent.target.value === '') {
+        options.value = [...modelValue.value]
+        return
+      }
 
       loading.value = true
       await new Promise(resolve => setTimeout(resolve, 500))
-      fetch('https://randomuser.me/api/?results=5')
-        .then(async result => {
-          const userResult = ((await result.json()) as any).results.map(
-            ({ name: { first, last } }) => first + ' ' + last,
-          )
-          users.value = selectedUser.value === null ? userResult : [selectedUser.value].concat(userResult)
-        })
-        .finally(() => {
-          loading.value = false
-        })
+      options.value = userNames.value.filter(name => name.includes(inputEvent.target.value))
+      loading.value = false
     }
 
     return {
-      selectedUser,
-      users,
+      modelValue,
+      options,
       loading,
       handleInput,
     }
